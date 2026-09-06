f9ebfb2f9b9c595aa89f2f56163178d17dcd6507
JoshuaTheNewUser <mediomayorpudu2021@gmail.com>
2026-09-06 19:12:24 -0300

    Update Settings.ts

--- src/ui/pages/Settings.ts
+++ src/ui/pages/Settings.ts
@@ -207,6 +207,7 @@ export const settingsInfo: Record<string, any> = {
       { label: "Wii U (default)", value: BodyType.WiiU },
       { label: "Switch", value: BodyType.Switch, disabled: true },
       { label: "Miitomo", value: BodyType.Miitomo },
+      { label: "Baby", value: BodyType.Baby },
     ],
   },
   bodyModelHands: {
